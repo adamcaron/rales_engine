56cfc57582a5ced16e662eb208656b0bafd1c71b
Adam Caron <adamwcaron@gmail.com>
2015-09-15 15:30:06 -0600

    Add test for items#show

--- spec/controllers/api/v1/items_controller_spec.rb
+++ spec/controllers/api/v1/items_controller_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::ItemsController, type: :controller do
+  scenario "#show" do
+    Item.create(name: "Thing", description: "Awesome", unit_price: "100000.00")
 
+    get :show, format: :json, id: Item.first.id
+
+    item = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to have_http_status(:success)
+    expect(item[:name]).to eq("Thing")
+    expect(item[:description]).to eq("Awesome")
+    expect(item[:unit_price]).to eq("100000.00")
+  end
 end
