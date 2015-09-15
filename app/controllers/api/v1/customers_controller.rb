3e13644c364ba898096c91af953394d6761d554c
Adam Caron <adamwcaron@gmail.com>
2015-09-14 18:37:15 -0600

    Add 'Show Record' endpoint for all resources

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -1,3 +1,15 @@
 class Api::V1::CustomersController < ApplicationController
   respond_to :json
+
+  def show
+    respond_with Customer.find_by(id: params[:id])
+  end
 end
+
+
+
+# show .find_by
+
+# find .find_by
+
+# find_all  .where
