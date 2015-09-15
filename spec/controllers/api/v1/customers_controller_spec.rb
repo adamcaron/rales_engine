0991768fbee7a4eb98d7d4dfe5e4725b2d36f723
Adam Caron <adamwcaron@gmail.com>
2015-09-15 15:50:15 -0600

    Test customers#find
    refactor 'find' to use params

--- spec/controllers/api/v1/customers_controller_spec.rb
+++ spec/controllers/api/v1/customers_controller_spec.rb
@@ -12,4 +12,18 @@ RSpec.describe Api::V1::CustomersController, type: :controller do
     expect(customer[:first_name]).to eq("John")
     expect(customer[:last_name]).to eq("Doe")
   end
+
+  scenario "#find" do
+    Customer.create(first_name: "John", last_name: "Doe")
+
+    get :find, format: :json, first_name: Customer.last.first_name
+    customer = JSON.parse(response.body, symbolize_names: true)
+    expect(response).to have_http_status(:success)
+    expect(customer[:first_name]).to eq("John")
+
+    get :find, format: :json, last_name: Customer.last.last_name
+    customer = JSON.parse(response.body, symbolize_names: true)
+    expect(response).to have_http_status(:success)
+    expect(customer[:last_name]).to eq("Doe")
+  end
 end
