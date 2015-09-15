4aa885f166ff8375b4bce0ab4c436bfab16a6a91
Adam Caron <adamwcaron@gmail.com>
2015-09-15 16:51:11 -0600

    Test transactions#find

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -6,15 +6,7 @@ class Api::V1::TransactionsController < ApplicationController
   end
 
   def find
-    if params.include?("credit_card_number")
-      respond_with Transaction.find_by(credit_card_number: params[:credit_card_number])
-    elsif params.include?("result")
-      respond_with Transaction.find_by(result: params[:result])
-    elsif params.include?("invoice_id")
-      respond_with Transaction.find_by(invoice_id: params[:invoice_id])
-    else
-      respond_with Transaction.find_by(id: params[:id])
-    end
+    respond_with Transaction.find_by(transaction_params)
   end
 
   def find_all
@@ -25,5 +17,11 @@ class Api::V1::TransactionsController < ApplicationController
     else params.include?("invoice_id")
       respond_with Transaction.where(invoice_id: params[:invoice_id])
     end
+  end
+
+  private
+
+  def transaction_params
+    params.permit(:credit_card_number, :result, :invoice_id)
   end
 end
--- spec/controllers/api/v1/transactions_controller_spec.rb
+++ spec/controllers/api/v1/transactions_controller_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::TransactionsController, type: :controller do
+  scenario "#show" do
+    customer    = Customer.create(first_name: "Joe", last_name: "Shmo")
+    merchant    = Merchant.create(name: "Alfonse Capone")
+    invoice     = Invoice.create(status: "shipped", customer_id: customer.id, merchant_id: merchant.id)
+    transaction = Transaction.create(credit_card_number: "4654405418249632", result: "success", invoice_id: invoice.id)
 
+    get :show, format: :json, id: Transaction.last.id
+
+    json_transaction = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to have_http_status(:success)
+    expect(json_transaction[:credit_card_number]).to eq(transaction.credit_card_number)
+    expect(json_transaction[:result]).to eq(transaction.result)
+    expect(json_transaction[:invoice_id]).to eq(transaction.invoice_id)
+  end
+
+  scenario "#find" do
+    customer    = Customer.create(first_name: "Joe", last_name: "Shmo")
+    merchant    = Merchant.create(name: "Alfonse Capone")
+    invoice     = Invoice.create(status: "shipped", customer_id: customer.id, merchant_id: merchant.id)
+    transaction = Transaction.create(credit_card_number: "4654405418249632", result: "success", invoice_id: invoice.id)
+
+    get :find, format: :json, credit_card_number: transaction.credit_card_number
+    json_transaction = JSON.parse(response.body, symbolize_names: true)
+    expect(json_transaction[:id]).to eq(transaction.id)
+
+    get :find, format: :json, result: transaction.result
+    json_transaction = JSON.parse(response.body, symbolize_names: true)
+    expect(json_transaction[:id]).to eq(transaction.id)
+
+    get :find, format: :json, invoice_id: transaction.invoice_id
+    json_transaction = JSON.parse(response.body, symbolize_names: true)
+    expect(json_transaction[:id]).to eq(transaction.id)
+  end
 end
