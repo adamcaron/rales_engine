a196564128e4c30341e7bd010013ee10ae047bc4
Adam Caron <adamwcaron@gmail.com>
2015-09-15 16:40:51 -0600

    Test invoice_items#find

--- app/controllers/api/v1/invoice_items_controller.rb
+++ app/controllers/api/v1/invoice_items_controller.rb
@@ -6,17 +6,7 @@ class Api::V1::InvoiceItemsController < ApplicationController
   end
 
   def find
-    if params.include?("quantity")
-      respond_with InvoiceItem.find_by(quantity: params[:quantity])
-    elsif params.include?("unit_price")
-      respond_with InvoiceItem.find_by(unit_price: params[:unit_price])
-    elsif params.include?("item_id")
-      respond_with InvoiceItem.find_by(item_id: params[:item_id])
-    elsif params.include?("invoice_id")
-      respond_with InvoiceItem.find_by(invoice_id: params[:invoice_id])
-    else
-      respond_with InvoiceItem.find_by(id: params[:id])
-    end
+    respond_with InvoiceItem.find_by(invoice_item_params)
   end
 
   def find_all
@@ -29,5 +19,11 @@ class Api::V1::InvoiceItemsController < ApplicationController
     else params.include?("invoice_id")
       respond_with InvoiceItem.where(invoice_id: params[:invoice_id])
     end
+  end
+
+  private
+
+  def invoice_item_params
+    params.permit(:id, :quantity, :unit_price, :item_id, :invoice_id)
   end
 end
--- spec/controllers/api/v1/invoice_items_controller_spec.rb
+++ spec/controllers/api/v1/invoice_items_controller_spec.rb
@@ -18,4 +18,28 @@ RSpec.describe Api::V1::InvoiceItemsController, type: :controller do
     expect(invoice_item[:item_id]).to eq(item.id)
     expect(invoice_item[:invoice_id]).to eq(invoice.id)
   end
+
+  scenario "#find" do
+    customer     = Customer.create(first_name: "Joe", last_name: "Shmo")
+    merchant     = Merchant.create(name: "Alfonse Capone")
+    invoice      = Invoice.create(status: "shipped", customer_id: customer.id, merchant_id: merchant.id)
+    item         = Item.create(name: "Thing", description: "Awesome", unit_price: "100000.00", merchant_id: merchant.id)
+    invoice_item = InvoiceItem.create(quantity: 2, unit_price: "100000.00", item_id: item.id, invoice_id: invoice.id)
+
+    get :find, format: :json, quantity: invoice_item.quantity
+    json_invoice_item = JSON.parse(response.body, symbolize_names: true)
+    expect(json_invoice_item[:id]).to eq(invoice_item.id)
+
+    get :find, format: :json, unit_price: invoice_item.unit_price
+    json_invoice_item = JSON.parse(response.body, symbolize_names: true)
+    expect(json_invoice_item[:id]).to eq(invoice_item.id)
+
+    get :find, format: :json, item_id: invoice_item.item_id
+    json_invoice_item = JSON.parse(response.body, symbolize_names: true)
+    expect(json_invoice_item[:id]).to eq(invoice_item.id)
+
+    get :find, format: :json, invoice_id: invoice_item.invoice_id
+    json_invoice_item = JSON.parse(response.body, symbolize_names: true)
+    expect(json_invoice_item[:id]).to eq(invoice_item.id)
+  end
 end
