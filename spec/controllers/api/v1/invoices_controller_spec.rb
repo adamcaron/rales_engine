65c693e099b3e54e3bb66ba316513b6a128ecfb8
Adam Caron <adamwcaron@gmail.com>
2015-09-15 15:30:51 -0600

    Test for invoices#show

--- spec/controllers/api/v1/invoices_controller_spec.rb
+++ spec/controllers/api/v1/invoices_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::InvoicesController, type: :controller do
+  scenario "#show" do
+    customer = Customer.create(first_name: "Joe", last_name: "Shmo")
+    merchant = Merchant.create(name: "Alfonse Capone")
+    Invoice.create(status: "shipped", customer_id: customer.id, merchant_id: merchant.id)
 
+    get :show, format: :json, id: Invoice.first.id
+
+    invoice = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to have_http_status(:success)
+    expect(invoice[:status]).to eq("shipped")
+    expect(invoice[:customer_id]).to eq(customer.id)
+    expect(invoice[:merchant_id]).to eq(merchant.id)
+  end
 end
