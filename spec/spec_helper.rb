4143fd8695607de91a114467b2487d421db55e50
Adam Caron <adamwcaron@gmail.com>
2015-09-15 15:35:31 -0600

    Remove FactoryGirl

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,9 +2,6 @@ require 'simplecov'
 SimpleCov.start 'rails'
 
 RSpec.configure do |config|
-
-  require 'support/factory_girl'
-
   require 'database_cleaner'
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
