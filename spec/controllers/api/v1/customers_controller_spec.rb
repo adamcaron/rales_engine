770e6db541ae08da65cc761d0c1865979abe51eb
Adam Caron <adamwcaron@gmail.com>
2015-09-15 15:33:38 -0600

    Test customers#show

--- spec/controllers/api/v1/customers_controller_spec.rb
+++ spec/controllers/api/v1/customers_controller_spec.rb
@@ -2,11 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::CustomersController, type: :controller do
   scenario "#show" do
-    customer = create(:customer)
+    Customer.create(first_name: "John", last_name: "Doe")
+
     get :show, format: :json, id: Customer.first.id
 
+    customer = JSON.parse(response.body, symbolize_names: true)
+
     expect(response).to have_http_status(:success)
-    expect(customer.first_name).to eq("John")
-    expect(customer.last_name).to eq("Doe")
+    expect(customer[:first_name]).to eq("John")
+    expect(customer[:last_name]).to eq("Doe")
   end
 end
