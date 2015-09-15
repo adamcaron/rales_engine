d60f1000f566fcd42638dadad343684e530985b4
Adam Caron <adamwcaron@gmail.com>
2015-09-15 15:31:48 -0600

    Test merchants#show

--- spec/controllers/api/v1/merchants_controller_spec.rb
+++ spec/controllers/api/v1/merchants_controller_spec.rb
@@ -1,4 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::MerchantsController, type: :controller do
+  scenario "#show" do
+    Merchant.create(name: "Lemony Snicket")
+
+    get :show, format: :json, id: Merchant.first.id
+
+    merchant = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to have_http_status(:success)
+    expect(merchant[:name]).to eq("Lemony Snicket")
+  end
 end
