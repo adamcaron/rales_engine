8911c91ace14d12f7dcf4b1bf4bf79c396597da6
Adam Caron <adamwcaron@gmail.com>
2015-09-16 20:15:42 -0600

    Add items#most_revenue

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,8 @@ class Item < ActiveRecord::Base
   belongs_to :merchant
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
+
+  def revenue
+    invoices.successful.sum('invoice_items.quantity * invoice_items.unit_price')
+  end
 end
